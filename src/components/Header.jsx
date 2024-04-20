c8d65c6ae6090cfb95f4b5a4116e8eebb60e742b
jose-join <70683227@continental.edu.pe>
2024-04-20 18:25:20 -0500

    Se Agrego tailwind css para el diseno

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
+    <div className='text-justify text-blue-600'>
       <h1>sheLead - React + Vite - Header</h1>
     </div>
   )
