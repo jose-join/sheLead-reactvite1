0aa26841ba343cb80f3a932c391bc8bfd51e340f
jose-join <70683227@continental.edu.pe>
2024-05-02 02:32:09 -0500

    -

---
+++ src/page/Empleos.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Empleos = () => {
+  return (
+    <div>Empleos</div>
+  )
+}
+
+export default Empleos
---
+++ src/page/PaginaInicio.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PaginaInicio = () => {
+  return (
+    <div>PaginaInicio</div>
+  )
+}
+
+export default PaginaInicio
